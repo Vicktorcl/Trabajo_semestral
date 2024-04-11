8cf0ef84851a803714ed6c07f661d249f5636815
Vicktorcl <victorrigocl@gmail.com>
2024-04-11 19:28:00 -0400

    añadido inicio cliente, admin, arreglo footers, navbar_cliente

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ if (document.getElementById('menu')) {
   });
 
 };
+
+if (document.getElementById('menu_cliente')) {
+
+  fetch('navbar_cliente.html').then(response => {
+
+      return response.text();
+
+  }).then(htmlContent => {
+
+  document.getElementById('menu_cliente').innerHTML = htmlContent;
+
+  window.scrollTo(0, 0);
+
+  });
+
+};
